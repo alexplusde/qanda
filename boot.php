b74d0c841207cb88c308444f7f4bcf767f51d599
Alexander Walther <github.com@alexplus.de>
2020-07-24 09:25:26 +0200

    choose editor, version bump

--- boot.php
+++ boot.php
@@ -8,3 +8,15 @@ rex_yform_manager_dataset::setModelClass(
     'rex_qanda_category',
     qanda_category::class
 );
+
+if (rex::isBackend() && rex_be_controller::getCurrentPage() == "qanda/qanda") {
+
+    rex_extension::register('OUTPUT_FILTER', function(rex_extension_point $ep){
+        $suchmuster = 'class="###qanda-settings-editor###"';
+        $ersetzen = rex_config::get("qanda", "editor");
+        $ep->setSubject(str_replace($suchmuster, $ersetzen, $ep->getSubject()));
+    });
+}
+
+// class="form-control cke5-editor" name="FORM[data_edit-rex_qanda][1]" id="yform-data_edit-rex_qanda-field-1" rows="10" data-lang="de" data-profile="across_inline"
+// {"class":"form-control cke5-editor","data-lang":"de","data-profile":"across_inline"}
--- pages/qanda.settings.php
+++ pages/qanda.settings.php
@@ -1,11 +1,12 @@
 <?php
 
-$addon = rex_addon::get('events');
+$addon = rex_addon::get('qanda');
 
 $form = rex_config_form::factory($addon->name);
 
 $field = $form->addInputField('text', 'editor', null, ['class' => 'form-control']);
 $field->setLabel(rex_i18n::msg('qanda_editor'));
+$field->setNotice('z.B. <code>class="form-control cke5-editor" data-lang="de" data-profile="default"</code>');
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
