1fb4dbe4fcf07245f97c38c3974bd4c74d1c9e40
Ingo Winter <IngoWinter@users.noreply.github.com>
2021-05-11 21:19:18 +0200

    Update qanda.php

--- lib/qanda.php
+++ lib/qanda.php
@@ -15,7 +15,7 @@ class qanda extends \rex_yform_manager_dataset
 
     public function getAnswerAsPlaintext() :string
     {
-        return strip_tags($this->getValue("answer"));
+        return str_replace('"', '\"', strip_tags($this->getValue("answer")));
     }
     
     public function getQuestion()
