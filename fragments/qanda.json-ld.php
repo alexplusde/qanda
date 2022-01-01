a44274409697e57caf3463181c690b96d1ae9c83
Alexander Walther <github.com@alexplus.de>
2022-01-01 18:55:52 +0100

    fix error

--- fragments/qanda.json-ld.php
+++ fragments/qanda.json-ld.php
@@ -3,26 +3,26 @@
     "@context": "https://schema.org",
     "@type": "Question",
 
-    "name": <?= json_encode($this->question->getQuestion(),  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK); ?>,
-    "text": <?= json_encode($this->question->getQuestion(),  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK); ?>,
+    "name": <?= json_encode($this->question->getQuestion(), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK); ?> ,
+    "text": <?= json_encode($this->question->getQuestion(), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK); ?> ,
 
     "answerCount": 1,
     "dateCreated": "<?= htmlentities($this->question->getValue('createdate'), ENT_QUOTES, 'UTF-8'); ?>",
     "author": {
       "@type": "Person",
 
-      "name": <?= json_encode($this->question->getAuthor(),  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK); ?>
+      "name": <?= json_encode($this->question->getAuthor(), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK); ?>
     },
     "acceptedAnswer": {
       "@type": "Answer",
-      "text": <?= json_encode($this->getValue('answer'),  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK); ?>,
+      "text": <?= json_encode($this->question->getValue('answer'), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK); ?> ,
 
       "upvoteCount": 0,
       "url": "<?= htmlentities($this->question->getUrl(), ENT_QUOTES, 'UTF-8'); ?>",
       "dateCreated": "<?= htmlentities($this->question->getValue('updatedate'), ENT_QUOTES, 'UTF-8'); ?>",
       "author": {
         "@type": "Person",
-        "name": <?= json_encode($this->question->getAuthor(),  JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK); ?>
+        "name": <?= json_encode($this->question->getAuthor(), JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK); ?>
 
       }
     }
