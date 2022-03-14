eda0d845b07c869111ac530490de72885ee35a49
Alexander Walther <aw@alexplus.de>
2022-03-14 14:42:16 +0100

    enhance url and json methods
    thx @tyrant88

--- lib/qanda.php
+++ lib/qanda.php
@@ -32,15 +32,21 @@ class qanda extends \rex_yform_manager_dataset
     }
     public function getUrl()
     {
-        return $this->getValue("url");
+        if (rex_addon::get("yrewrite") && rex_addon::get("yrewrite")->isAvailable()) {
+            $host = rex_yrewrite::getFullUrlByArticleId(rex_article::getCurrentId(), rex_clang::getCurrentId());
+        } else {
+            $host = rtrim(rex::getServer(),'/').rex_getUrl(rex_article::getCurrentId(), rex_clang::getCurrentId());
+        }
+
+        return rtrim($host,'/').'/#question-header-'. $this->getId();
     }
-    public function showJsonLd($question)
+    public static function showJsonLd($question)
     {
         $fragment = new rex_fragment;
         $fragment->setVar("question", $question);
         return $fragment->parse('qanda.json-ld.php');
     }
-    public function showFAQPage($questions)
+    public static function showFAQPage($questions)
     {
         $fragment = new rex_fragment;
         $fragment->setVar("questions", $questions);
--- lib/qanda_category.php
+++ lib/qanda_category.php
@@ -5,4 +5,10 @@ class qanda_category extends \rex_yform_manager_dataset
     {
         return $this->getValue("name");
     }
+
+    public function getAllQuestions()
+    {
+        return qanda::query()->whereListContains('category_ids',$this->id)
+            ->find();
+    }
 }
