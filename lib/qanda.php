62fc77547a74ce56e8bb032a8f6c4d9ca3611a9a
Alexander Walther <github.com@alexplus.de>
2022-08-03 16:58:32 +0200

    Update qanda.php

--- lib/qanda.php
+++ lib/qanda.php
@@ -30,7 +30,7 @@ class qanda extends \rex_yform_manager_dataset
     {
         return $this->getValue("author");
     }
-    public function getUrl()
+    public function getUrl($param = "question-header-")
     {
         if (rex_addon::get("yrewrite") && rex_addon::get("yrewrite")->isAvailable()) {
             $host = rex_yrewrite::getFullUrlByArticleId(rex_article::getCurrentId(), rex_clang::getCurrentId());
@@ -38,7 +38,7 @@ class qanda extends \rex_yform_manager_dataset
             $host = rtrim(rex::getServer(),'/').rex_getUrl(rex_article::getCurrentId(), rex_clang::getCurrentId());
         }
 
-        return rtrim($host,'/').'/#question-header-'. $this->getId();
+        return rtrim($host,'/').'#'.$param. $this->getId();
     }
     public static function showJsonLd($question)
     {
