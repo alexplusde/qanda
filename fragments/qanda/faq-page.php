13e8a7da840c76964036022df5bbf8cfd69d3c25
Alexander Walther <aw@alexplus.de>
2022-12-07 01:05:18 +0100

    hotfix lang + faqpage

--- fragments/qanda/faq-page.php
+++ fragments/qanda/faq-page.php
@@ -1,20 +1,18 @@
-<?php
 <div class="qanda container">
-
-  <?php
+	<?php
 foreach (qanda::getAll() as $question) {
     ?>
 
-  <details class="card">
-    <summary class="qanda-question card-body card-title h4">
-      <?= $question->getQuestion() ?>
-    </summary>
-    <div class="qanda-answer card-body">
-      <?= $question->getAnswer() ?>
-    </div>
-  </details>
-  <?php
+	<details class="card">
+		<summary class="qanda-question card-body card-title h4">
+			<?= $question->getQuestion() ?>
+		</summary>
+		<div class="qanda-answer card-body">
+			<?= $question->getAnswer() ?>
+		</div>
+	</details>
+	<?php
     echo $question->showJsonLd($question);
 }
-?>
+	?>
 </div>
