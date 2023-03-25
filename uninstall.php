81a969ee98723188326b87a495c77c720b66ee88
Alexander Walther <github.com@alexplus.de>
2023-03-25 09:42:54 +0100

    Create uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,7 @@
+<?php
+rex_config::removeNamespace("qanda");
+
+if (rex_addon::get('yform')->isAvailable() && !rex::isSafeMode()) {
+  rex_yform_manager_table_api::removeTable('rex_qanda');
+  rex_yform_manager_table_api::removeTable('rex_qanda_category');
+}
